33e36ffa5294568bf9ccfdb14192f151e2a940cb
Francisco B. dos Santos <francfbs@gmail.com>
2020-08-05 13:36:39 -0300

    feat: ServiceBase implementation

---
+++ src/LabTest.Domain/Services/ServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTest.Domain.Services
+{
+    public abstract class ServiceBase
+    {
+        public ServiceResult OkResult(object data)
+        {
+            return new ServiceResult(true, data);
+        }
+
+        public ServiceResult FailResult(object data)
+        {
+            return new ServiceResult(false, data);
+        }
+    }
+}
